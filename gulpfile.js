e400681fdc7be1cd59aa65c358f0178bce615d91
yamamoto syo <syo19961113@gmail.com>
2020-11-10 22:41:33 +0900

    gulp setting

--- gulpfile.js
+++ gulpfile.js
@@ -35,12 +35,12 @@ const { src, dest, watch } = require('gulp');
 
 // sassコンパイル
 const cssTask = () => {
-    return src('./src/sass/**/**', {sourcemaps: true})
+    return src('./src/sass/**/*.scss', {sourcemaps: true})
         .pipe(sassGlob())
         .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
         .pipe(autoprefixer({cascade:false}))
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(dest('../css', {sourcemaps: true}))
+        .pipe(dest('./css', {sourcemaps: true}))
 }
 
 // jsコンパイル
@@ -77,7 +77,7 @@ const htmlTask = () => {
 }
 
 const keiryoWatch = () => {
-    watch('./src/sass/**', cssTask)
+    watch('./src/sass/**/*.scss', cssTask)
     watch('./src/js/**', jsTask)
     watch('./src/images/**', imgTask)
     watch('./src/html/**', htmlTask)
--- js/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-console.log('aaa');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7QUFBQUEsT0FBTyxDQUFDQyxHQUFSLENBQVksS0FBWiIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdhYWEnKTtcbiJdLCJmaWxlIjoibWFpbi5qcyJ9
